0d212297eaebf4fc806a48f2232cd8eb88bbaebd
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-05-01 22:11:21 +0530

    fix: format code

--- tests/e2e/integration/behavior.spec.js
+++ tests/e2e/integration/behavior.spec.js
@@ -1,9 +1,13 @@
-describe('behavior', () => {
-  it('is possible to enter text to the textarea intended to receive input CSS snippet', () => {
-    const cssSnippet = 'padding: 10px;';
-    cy.visit('/')
-    .get('.code--input')
-    .type(cssSnippet)
-    .should('have.value', cssSnippet)
-  })
+describe("behavior", () => {
+  it("is possible to enter text to the textarea intended to receive input CSS snippet", () => {
+    const cssSnippet = "padding: 10px;";
+    cy.visit("/")
+      .get(".code--input")
+      .type(cssSnippet)
+      .should("have.value", cssSnippet);
+  });
+
+  it("expects to find readonly attribute associated with the textarea intended to display the result", () => {
+    cy.visit("/").get(".code--output").should("have.attr", "readonly");
+  });
 });
