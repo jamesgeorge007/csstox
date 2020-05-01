91fc8062c7c29506e570a8f6bb3c7241d290306b
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-05-01 21:59:34 +0530

    tests: assert for default value showing up as part of the select box

--- tests/e2e/integration/behavior.spec.js
+++ tests/e2e/integration/behavior.spec.js
@@ -2,4 +2,10 @@ describe('basic workflow', () => {
   it('renders without crashing', () => {
     cy.visit('/')
   })
+
+  it('expects to find the select box defaulting to React Native', () => {
+    cy.visit('/')
+    .get('.select')
+    .should('have.value', 'React Native')
+  })
 })
