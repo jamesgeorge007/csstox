05549ed74440d2584e096b59a9927ac106d31789
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-05-01 22:37:15 +0530

    tests: test case to ensure an input css snippet generates the JSS equivalent

--- tests/e2e/integration/behavior.spec.js
+++ tests/e2e/integration/behavior.spec.js
@@ -22,4 +22,12 @@ describe("behavior", () => {
     };
     cy.visit("/").get(".code--input").type(inputCSSRule).get(".code--output").should("have.value", JSON.stringify(result, null, 2));
   });
+
+  it('converts an input CSS snippet to the JSS equivalent', () => {
+    const inputCSSRule = 'margin: 5px 7px 2px;';
+    const result = {
+      "margin": "5px 7px 2px"
+    };
+    cy.visit("/").get('select').select('JSS').get(".code--input").type(inputCSSRule).get(".code--output").should("have.value", JSON.stringify(result, null, 2));
+  });
 });
