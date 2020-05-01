8fe42e3e2d8342f7a2797caa70da6521f17b7c51
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-05-01 22:11:21 +0530

    fix: format code

--- tests/e2e/integration/basic.spec.js
+++ tests/e2e/integration/basic.spec.js
@@ -1,11 +1,9 @@
-describe('basic workflow', () => {
-  it('renders without crashing', () => {
-    cy.visit('/')
-  })
+describe("basic workflow", () => {
+  it("renders without crashing", () => {
+    cy.visit("/");
+  });
 
-  it('expects to find the select box defaulting to React Native', () => {
-    cy.visit('/')
-    .get('.select')
-    .should('have.value', 'React Native')
-  })
-})
+  it("expects to find the select box defaulting to React Native", () => {
+    cy.visit("/").get(".select").should("have.value", "React Native");
+  });
+});
