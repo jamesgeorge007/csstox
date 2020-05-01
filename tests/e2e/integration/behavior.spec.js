fafddbbfae67ea980f45ac03ea8f899edef857ca
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-05-01 22:30:09 +0530

    tests: test case to ensure an input css snippet generates the React Native equivalent

--- tests/e2e/integration/behavior.spec.js
+++ tests/e2e/integration/behavior.spec.js
@@ -10,4 +10,16 @@ describe("behavior", () => {
   it("expects to find readonly attribute associated with the textarea intended to display the result", () => {
     cy.visit("/").get(".code--output").should("have.attr", "readonly");
   });
+
+  it('converts an input CSS snippet to the React Native equivalent', () => {
+    const inputCSSRule = 'transform: translate(10px, 5px) scale(5);';
+    const result = {
+      transform: [
+        { scale: 5 },
+        { translateY: 5 },
+        { translateX: 10 },
+      ]
+    };
+    cy.visit("/").get(".code--input").type(inputCSSRule).get(".code--output").should("have.value", JSON.stringify(result, null, 2));
+  });
 });
