30884074ceefb6c2ee9fc1b5d6c1807a1377ef21
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-05-01 22:50:43 +0530

    fix: format code

--- tests/e2e/integration/behavior.spec.js
+++ tests/e2e/integration/behavior.spec.js
@@ -11,31 +11,41 @@ describe("behavior", () => {
     cy.visit("/").get(".code--output").should("have.attr", "readonly");
   });
 
-  it('converts an input CSS snippet to the React Native equivalent', () => {
-    const inputCSSRule = 'transform: translate(10px, 5px) scale(5);';
+  it("converts an input CSS snippet to the React Native equivalent", () => {
+    const inputCSSRule = "transform: translate(10px, 5px) scale(5);";
     const result = {
-      transform: [
-        { scale: 5 },
-        { translateY: 5 },
-        { translateX: 10 },
-      ]
+      transform: [{ scale: 5 }, { translateY: 5 }, { translateX: 10 }],
     };
-    cy.visit("/").get(".code--input").type(inputCSSRule).get(".code--output").should("have.value", JSON.stringify(result, null, 2));
+    cy.visit("/")
+      .get(".code--input")
+      .type(inputCSSRule)
+      .get(".code--output")
+      .should("have.value", JSON.stringify(result, null, 2));
   });
 
-  it('converts an input CSS snippet to the JSS equivalent', () => {
-    const inputCSSRule = 'margin: 5px 7px 2px;';
+  it("converts an input CSS snippet to the JSS equivalent", () => {
+    const inputCSSRule = "margin: 5px 7px 2px;";
     const result = {
-      "margin": "5px 7px 2px"
+      margin: "5px 7px 2px",
     };
-    cy.visit("/").get('select').select('JSS').get(".code--input").type(inputCSSRule).get(".code--output").should("have.value", JSON.stringify(result, null, 2));
+    cy.visit("/")
+      .get("select")
+      .select("JSS")
+      .get(".code--input")
+      .type(inputCSSRule)
+      .get(".code--output")
+      .should("have.value", JSON.stringify(result, null, 2));
   });
 
-  it('shows an error message for invalid CSS snippet', () => {
-    const inputCSSRule = 'margin: 5';
-    const result = `Error translating CSS`
-    cy.visit("/").get('.code--input').type(inputCSSRule).get(".code--output").should((el) => {
-      expect(el).to.contain(result);
-    });
+  it("shows an error message for invalid CSS snippet", () => {
+    const inputCSSRule = "margin: 5";
+    const result = `Error translating CSS`;
+    cy.visit("/")
+      .get(".code--input")
+      .type(inputCSSRule)
+      .get(".code--output")
+      .should((el) => {
+        expect(el).to.contain(result);
+      });
   });
 });
